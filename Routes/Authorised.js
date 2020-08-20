dcb12626023985d53d39dcaee877f3b6843f7bae
Jared Evans <jarede@gmail.com>
2020-08-20 11:56:45 +0100

    Update Authorised.js

--- Routes/Authorised.js
+++ Routes/Authorised.js
@@ -1,6 +1,20 @@
 exports.generate = async function(req, res) {
-  res.render('generate', {
-    title: 'SSH Key Server - Generate a key pair',
-    username: 'hi'
-  });
+  data = {
+    '12354645': {
+      'name': 'bleep'
+    },
+    '12354635': {
+      'name': 'itunes'
+    },
+    '12314645': {
+      'name': 'amazon'
+    },
+  }
+  if (data) {
+    res.render('generate', {
+      title: 'SSH Key Server - Generate a key pair',
+      username: 'hi',
+      musicServices: data,
+    });
+  }
 }
